5db3285775506c7b4566d3ea1f9a135d82ffc6ab
SoupriceManiac <32708475+0423y@users.noreply.github.com>
2024-10-13 14:43:10 +0800

    feat：引入自研分库分表组件，更新库表语句，验证数据散列落库

---
+++ pfcl-infrastructure/src/main/java/com/pfc/pfcl/infrastructure/dao/IUserTakeActivityDao.java
@@ -0,0 +1,22 @@
+package com.pfc.pfcl.infrastructure.dao;
+
+import cn.bugstack.middleware.db.router.annotation.DBRouter;
+import com.pfc.pfcl.infrastructure.po.UserTakeActivity;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @description 用户领取活动表DAO
+ * @author ypf
+ */
+@Mapper
+public interface IUserTakeActivityDao {
+
+    /**
+     * 插入用户领取活动信息
+     *
+     * @param userTakeActivity 入参
+     */
+    @DBRouter(key = "uId")
+    void insert(UserTakeActivity userTakeActivity);
+
+}
